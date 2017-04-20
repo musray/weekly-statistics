c11048fa4c423365f531d9f22950ec6359f53b0c
musray <churui.ctec@gmail.com>
2017-04-20 19:00:36 +0800

    edited some components

--- src/DayEntry.js
+++ src/DayEntry.js
@@ -1,22 +1,58 @@
 import React from 'react';
 
-const Task = (props) => {
-    return (
-        <div>
-            <select name="project">
-                <option value="">placeholder</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-            </select>
-            <textarea name="description" cols="30" rows="10"/>
-            <input type="text"/>
-            <button onClick={() => props.remove(props.task.id)}>
-                删除
-            </button>
-        </div>
-    )
-};
+// const Task = (props) => {
+//     return (
+//         <div>
+//             <select name="project">
+//                 <option value="">placeholder</option>
+//                 <option value="1">1</option>
+//                 <option value="2">2</option>
+//                 <option value="3">3</option>
+//             </select>
+//             <textarea name="description" cols="30" rows="10"/>
+//             <input type="text"/>
+//             <button onClick={() => props.remove(props.task.id)}>
+//                 删除
+//             </button>
+//         </div>
+//     )
+// };
+
+class Task extends React.Component{
+    constructor (props) {
+        super(props);
+        this.state = {
+            id: '',
+            date: '',
+            personName: '',
+            project: '',
+            taskTarget: '',
+            description: '',
+            workload: '',
+            overtime: '',
+            duration: ''
+        };
+    }
+
+    render() {
+        let projectValue = this.state.project;
+        return (
+            <div>
+                <select name="project">
+                    <option value="">placeholder</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
+                <textarea name="description" cols="30" rows="10"/>
+                <input type="text"/>
+                <button onClick={() => props.remove(props.task.id)}>
+                    删除
+                </button>
+            </div>
+        )
+    }
+}
 
 const TaskList = (props) => {
     const taskNode = props.tasks.map(task => {
--- src/WeekEntry.js
+++ src/WeekEntry.js
@@ -2,20 +2,28 @@ import React from 'react';
 import DayEntry from './DayEntry';
 
 class WeekEntry extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
     handleSubmit(e) {
         e.preventDefault();
         console.log('用户提交');
     }
+
     render() {
         return (
             <form onSubmit={this.handleSubmit.bind(this)}>
-                <DayEntry title="mon"/>
-                <DayEntry title="tue"/>
-                <DayEntry title="wen"/>
-                <DayEntry title="thu"/>
-                <DayEntry title="fri"/>
-                <DayEntry title="sat"/>
-                <DayEntry title="sun"/>
+                <DayEntry
+                    title="mon"
+                    value={this.state.data}/>
+                {/*<DayEntry title="tue"/>*/}
+                {/*<DayEntry title="wen"/>*/}
+                {/*<DayEntry title="thu"/>*/}
+                {/*<DayEntry title="fri"/>*/}
+                {/*<DayEntry title="sat"/>*/}
+                {/*<DayEntry title="sun"/>*/}
                 <input type="submit" value="提交" />
             </form>
         );
